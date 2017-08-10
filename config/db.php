709aa0c26c88c80539efd91c6d0b35e8b41df233
rudmanmrrod <rudman22@hotmail.com>
2017-08-10 14:30:02 -0400

    Agregada bd del servidor

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     #'dsn' => 'mysql:host=localhost;dbname=legalitas',
-    #'dsn' => 'pgsql:host=ec2-107-20-188-239.compute-1.amazonaws.com;port=5432;dbname=d8n8ugs5dumbar',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=legalitas',
+    'dsn' => 'pgsql:host=ec2-107-20-188-239.compute-1.amazonaws.com;port=5432;dbname=d8n8ugs5dumbar',
+    #'dsn' => 'pgsql:host=localhost;port=5432;dbname=legalitas',
     'username' => 'postgres',
     'password' => '123456',
     'charset' => 'utf8',
