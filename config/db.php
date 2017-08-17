31d6355894cf1f7d479e7cc1fee0175488a38fe4
rudmanmrrod <rudman22@hotmail.com>
2017-08-17 16:31:27 -0400

    Añadido mostrar el nombre y apellido en la view de usuario

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@ return [
     #'dsn' => 'mysql:host=localhost;dbname=legalitas',
     'dsn' => 'pgsql:host=ec2-107-20-188-239.compute-1.amazonaws.com;port=5432;dbname=d8n8ugs5dumbar',
     #'dsn' => 'pgsql:host=localhost;port=5432;dbname=legalitas',
-    #'username' => 'root',
+    #'username' => 'postgres',
     'username' => 'snmkpbvjnhllin',
     #'password' => '123456',
     'password' => '243bef335b1ad5daf2c40b2dcf4bc44b7c47b719d478793f639ee1f3be8693e7',
