6c36223c55c9f9156c42db1d7e7fcc4dd79a762e
rudmanmrrod <rudman22@hotmail.com>
2017-08-17 16:15:00 -0400

    Añadido dsn para h

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=legalitas',
-    #'dsn' => 'pgsql:host=ec2-107-20-188-239.compute-1.amazonaws.com;port=5432;dbname=d8n8ugs5dumbar',
+    #'dsn' => 'mysql:host=localhost;dbname=legalitas',
+    'dsn' => 'pgsql:host=ec2-107-20-188-239.compute-1.amazonaws.com;port=5432;dbname=d8n8ugs5dumbar',
     #'dsn' => 'pgsql:host=localhost;port=5432;dbname=legalitas',
     #'username' => 'root',
     'username' => 'snmkpbvjnhllin',
