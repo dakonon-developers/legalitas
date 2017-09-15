5ee4646a43e32f46de0dcb41c9ffe957fd2418e7
Pablo González <pdonaire1@gmail.com>
2017-09-15 09:45:53 -0430

    agregando aggrement y subscription con paypal test

--- paypal_test_subscription_step1.php
+++ paypal_test_subscription_step1.php
@@ -12,6 +12,7 @@ use PayPal\Api\PatchRequest;
 use PayPal\Common\PayPalModel;
 use \PayPal\Api\Agreement;
 use \PayPal\Api\FundingInstrument;
+use PayPal\Api\CreditCardToken;
 use \PayPal\Api\Payer;
 header('Content-Type: application/json');
 // 2. Provide your Secret Key. Replace the given one with your app clientId, and Secret
@@ -28,13 +29,14 @@ echo "2\n";
 
 $creditCard = new \PayPal\Api\CreditCard();
 echo "3\n";
-$card = $creditCard->get('CARD-63W18615JB2380407LGWEAZI', $apiContext);
+$card = $creditCard->get('CARD-30L1581339390110YLG55XNA', $apiContext);
 echo "a\n";
 $agreement = new \PayPal\Api\Agreement();
 echo "b\n";
 $agreement->setName('Base Agreement for ')
   ->setDescription('Basic Agreement for ')
   ->setStartDate('2019-06-17T9:45:04Z');
+
 echo "c\n";
 $plan = new Plan();
 echo "d\n";
@@ -42,13 +44,20 @@ $plan->setId('P-8GT68861ER278184NHFYX7VA');
 echo "e\n";
 $agreement->setPlan($plan);
 echo "f\n";
+
+$creditCardToken = new CreditCardToken();
+$creditCardToken->setCreditCardId($card->getId());
 $fi = new FundingInstrument();
+$fi->setCreditCardToken($creditCardToken);
 echo "g\n";
 $fi->setCreditCard($card);
+
+
 echo "h\n";
 // Set payer to process credit card
 $payer = new Payer();
 echo "i\n";
+// $payer->setPaymentMethod("paypal");
 $payer->setPaymentMethod("credit_card")
   ->setFundingInstruments(array($fi));
 echo "j\n";
@@ -56,16 +65,40 @@ $agreement->setPayer($payer);
 echo "k\n";
 // $agreement = $agreement->create($apiContext);
 echo "l\n";
-echo "AGREEMENT: \n".$agreement."\n";
+
+// echo "AGREEMENT: \n".$agreement."\n";
 // json_encode($agreement, true);
 // print_r( get_object_vars($agreement) );
 // print_r($agreement->payer->funding_instruments);
 // $token = $agreement->payer;///["funding_instruments"]["links"][0];
-$authorizationCode = "EG1WryIO0cTSgFTT9bY0Y2Sm63r7tjtR4igKogqvsqFulOxutoO9SDEfVd-nw9j4qKpgJk9dkqqtFw3F";
-$refreshToken = \PayPal\Api\FuturePayment::getRefreshToken($authorizationCode, $apiContext);
-echo "$refreshToken:\n ".$refreshToken."\n";
-
+// $authorizationCode = "EG1WryIO0cTSgFTT9bY0Y2Sm63r7tjtR4igKogqvsqFulOxutoO9SDEfVd-nw9j4qKpgJk9dkqqtFw3F";
+// $refreshToken = \PayPal\Api\FuturePayment::getRefreshToken($authorizationCode, $apiContext);
+// echo "$refreshToken:\n ".$refreshToken."\n";
+$request = clone $agreement;
 try {
+    // Please note that as the agreement has not yet activated, we wont be receiving the ID just yet.
+    $agreement = $agreement->create($apiContext);
+    // ### Get redirect url
+    // The API response provides the url that you must redirect
+    // the buyer to. Retrieve the url from the $agreement->getApprovalLink()
+    // method
+    $approvalUrl = $agreement->getApprovalLink();
+// } catch (Exception $ex) {
+//     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+//     echo "Created Billing Agreement.", "Agreement", null, $request, $ex;
+//     exit(1);
+// }
+    echo "$agreement\n\n$approvalUrl";
+  } catch (\PayPal\Exception\PayPalConnectionException $ex) {
+    die($ex);
+    // echo "Created Billing Agreement.", "Agreement", null, $request, $ex;
+    die(1);
+  } catch (Exception $ex) {
+    die($ex);
+  }
+
+/**************************************************/
+/*try {
   // Execute agreement
   $agreement->execute("A21AAHGtD-WvugyMU7INSAGn24ta79aD1DiEs2Gljx2Xs173prEcuNlgh7ZBatoXJzKHQMmapZ7gFvBDi2Fn6ureK8gNSVjUQ", $apiContext);
 } catch (PayPal\Exception\PayPalConnectionException $ex) {
@@ -76,7 +109,7 @@ try {
   die($ex);
 }
 
-
+*/
 
 
   
---
+++ paypal_test_subscription_step2.php
@@ -0,0 +1,52 @@
+<?php
+// 1. Autoload the SDK Package. This will include all the files and classes to your autoloader
+//require __DIR__  . '/vendor/paypal/PayPal-PHP-SDK/autoload.php';
+require __DIR__ .'/PayPal-PHP-SDK/autoload.php';
+use PayPal\Api\ChargeModel;
+use PayPal\Api\Currency;
+use PayPal\Api\MerchantPreferences;
+use PayPal\Api\PaymentDefinition;
+use PayPal\Api\Plan;
+use PayPal\Api\Patch;
+use PayPal\Api\PatchRequest;
+use PayPal\Common\PayPalModel;
+use \PayPal\Api\Agreement;
+use \PayPal\Api\FundingInstrument;
+use \PayPal\Api\Payer;
+header('Content-Type: application/json');
+// 2. Provide your Secret Key. Replace the given one with your app clientId, and Secret
+// https://developer.paypal.com/webapps/developer/applications/myapps
+echo "1\n";
+
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        'AZl3I48baDm4BGsILA05icnn5UauIObxmUPJkRYzNBOIUwuFoJJEjswiFTSnc90yJPEVPdDioNp0-izK',     // ClientID
+        'EG1WryIO0cTSgFTT9bY0Y2Sm63r7tjtR4igKogqvsqFulOxutoO9SDEfVd-nw9j4qKpgJk9dkqqtFw3F'      // ClientSecret
+    )
+);
+
+  /*********************************************/
+  $token = "EC-21D186840J912114F";
+  $agreement = new \PayPal\Api\Agreement();
+  try {
+    // ## Execute Agreement
+    // Execute the agreement by passing in the token
+    $agreement->execute($token, $apiContext);
+  } catch (\PayPal\Exception\PayPalConnectionException $ex) {
+    // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+    echo "Executed an Agreement", "Agreement", $agreement->getId(), $token, $ex;
+    exit(1);
+  }
+
+echo "Executed an Agreement", "Agreement", $agreement->getId(), $token, $agreement;
+/*
+try {
+    $agreement = \PayPal\Api\Agreement::get($agreement->getId(), $apiContext);
+} catch (Exception $ex) {
+    // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+    echo "Get Agreement", "Agreement", null, null, $ex;
+    exit(1);
+}
+// NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+echo "Get Agreement", "Agreement", $agreement->getId(), null, $agreement;
+*/
