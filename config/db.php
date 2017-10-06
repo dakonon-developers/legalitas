5cb39dd7acb37ae75351b9d90367f2ed4132f38f
rudmanmrrod <rudman22@hotmail.com>
2017-10-06 08:36:11 -0400

    Config de bd en heroku

--- config/db.php
+++ config/db.php
@@ -3,15 +3,15 @@
 return [
     'class' => 'yii\db\Connection',
     //'dsn' => 'mysql:host=localhost;dbname=legalitas',
-    //'dsn' => 'pgsql:host=ec2-107-20-188-239.compute-1.amazonaws.com;port=5432;dbname=d8n8ugs5dumbar',
-    //'dsn' => 'pgsql:host=localhost;port=5432;dbname=legalitas_prod',
     //'username' => 'root',
-    //'username' => 'snmkpbvjnhllin',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=legalitas',
-    'username' => 'postgres',
     //'password' => '123',
-    'password' => '123456',
-    //'password' => '243bef335b1ad5daf2c40b2dcf4bc44b7c47b719d478793f639ee1f3be8693e7',
+    'dsn' => 'pgsql:host=ec2-107-20-188-239.compute-1.amazonaws.com;port=5432;dbname=d8n8ugs5dumbar',
+    'username' => 'snmkpbvjnhllin',
+    'password' => '243bef335b1ad5daf2c40b2dcf4bc44b7c47b719d478793f639ee1f3be8693e7',
+    //'dsn' => 'pgsql:host=localhost;port=5432;dbname=legalitas_prod',
+    //'dsn' => 'pgsql:host=localhost;port=5432;dbname=legalitas',
+    //'username' => 'postgres',
+    //'password' => '123456',
     'charset' => 'utf8',
 ];
     
