ee10889ea660aa48842d72f8e8ac76a4d7f0cc39
Pablo González <pdonaire1@gmail.com>
2017-10-27 21:08:30 -0430

    Nota agregada

--- views/consulta/preCreate.php
+++ views/consulta/preCreate.php
@@ -10,7 +10,7 @@ $this->title = 'Pagar servicio';
 <div class="igualas-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
+    <div><b>Nota:</b> si el usuario ya está subscrito a un plan, es sistema automáticamente lo desuscribirá de ese plan y le asignará este nuevo</div>
     <a href="<?= $approvalUrl ?>">Confirmar la operación</a>
 
 </div>
